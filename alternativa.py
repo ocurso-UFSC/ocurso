cb809e1920a5acf368e960ba0d0c4738d2e57f4f
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2021-08-04 23:04:59 -0300

    Criação alternativa.py
    Criação do alternativa.py, sua classe, seus atributos e método (ainda sem funcionamento)

---
+++ alternativa.py
@@ -0,0 +1,35 @@
+class Alternativa:
+    def __init__(self,
+                index: str,
+                descricao_alternativa: str,
+                status_alternativa: bool):
+            self.__index = index
+            self.__descricao_alternativa = descricao_alternativa
+            self.__status_alternativa = status_alternativa
+
+    @property
+    def index(self):
+        return self.__index
+
+    @property
+    def descricao_alternativa(self):
+        return self.__descricao_alternativa
+
+    @property
+    def status_alternativa(self):
+        return self.__status_alternativa
+
+    @index.setter
+    def index(self, index):
+        self.__index = index
+
+    @descricao_alternativa.setter
+    def descricao_alternativa(self, descricao_alternativa):
+        self.__descricao_alternativa = descricao_alternativa
+
+    @status_alternativa.setter
+    def status_alternativa(self, status_alternativa):
+        self.__status_alternativa = status_alternativa
+
+    def editar_descricao(self, descricao_alternativa):
+        ...
