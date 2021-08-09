32f0c60c503b209de3cb1de1d4ee8922e55b1f67
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2021-08-09 20:51:31 -0300

    Criação das Classes
    Apenas a criação padrão das classes:
    Curso, Usuário, Progresso.

---
+++ usuario.py
@@ -0,0 +1,59 @@
+from progresso import Progresso
+from curso import Curso
+
+
+class Usuario:
+    def __init__(
+                self,
+                nome: str,
+                email: str,
+                senha: str,
+                notas: list = [],
+                progresso: list = []):
+
+        self.__nome = nome
+        self.__email = email
+        self.__senha = senha
+        self.__notas = notas
+        self.__progresso = progresso
+
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def email(self):
+        return self.__email
+
+    @property
+    def senha(self):
+        return self.__senha
+
+    @property
+    def notas(self):
+        return self.__notas
+
+    @property
+    def progresso(self):
+        return self.__progresso
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @email.setter
+    def nome(self, email):
+        self.__email = email
+
+    @senha.setter
+    def nome(self, senha):
+        self.__senha = senha
+
+    @notas.setter
+    def nome(self, notas):
+        self.__notas = notas
+
+    @progresso.setter
+    def nome(self, progresso):
+        self.__progresso = progresso
