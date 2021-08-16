570e4128339b44c5443ff31d61cce55f26236261
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2021-08-15 22:19:56 -0300

    Atualizando conteudo.py
    simplificando a classe Conteudo

--- entidade/conteudo.py
+++ entidade/conteudo.py
@@ -1,18 +1,10 @@
 class Conteudo:
     def __init__(
             self,
-            numero_conteudo: int,
             descricao_conteudo: str,
-            link_conteudo: str,
-            checkbox: bool):
-        self.__numero_conteudo = numero_conteudo
+            link_conteudo: str,):
         self.__descricao_conteudo = descricao_conteudo
         self.__link_conteudo = link_conteudo
-        self.__checkbox = checkbox
-
-    @property
-    def numero_conteudo(self):
-        return self.__numero_conteudo
 
     @property
     def descricao_conteudo(self):
@@ -22,14 +14,6 @@ class Conteudo:
     def link_conteudo(self):
         return self.__link_conteudo
 
-    @property
-    def checkbox(self):
-        return self.__checkbox
-
-    @numero_conteudo.setter
-    def numero_conteudo(self, numero_conteudo):
-        self.__numero_conteudo = numero_conteudo
-
     @descricao_conteudo.setter
     def descricao_conteudo(self, descricao_conteudo):
         self.__descricao_conteudo = descricao_conteudo
@@ -37,7 +21,3 @@ class Conteudo:
     @link_conteudo.setter
     def link_conteudo(self, link_conteudo):
         self.__link_conteudo = link_conteudo
-
-    @checkbox.setter
-    def checkbox(self, checkbox):
-        self.__checkbox = checkbox
