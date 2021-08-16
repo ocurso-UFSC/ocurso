da99a11469faa394d6c5d306a74f17c0bc94c948
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2021-08-16 14:29:22 -0300

    Controlador Login Funcionando

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from controle.ControladorSistema import ControladorSistema
 from controle.ControladorLogin import ControladorLogin
 
 if __name__ == "__main__":
-    ControladorLogin().inicializa_sistema()
+    ControladorSistema().inicializa_sistema()
