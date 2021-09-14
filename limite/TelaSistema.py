0a5b24e8b0603cc391e17f38214c447acf246ee2
Henrique Carvalho <henrique.scarvalho01@gmail.com>
2021-09-14 16:35:38 -0300

    Iniciando as telas

--- limite/TelaSistema.py
+++ limite/TelaSistema.py
@@ -1,14 +1,28 @@
-class TelaSistema:
+import PySimpleGUI as sg
+class TelaSistema():
     def tela_opcoes(self):
-        print("-------- oCurso ---------")
-        print("Escolha sua opcao")
-        print("1 - Usuario")
-        print("2 - Cursos")
-        print("3 - Progresso")
-        print("9 - AUTOMATICO")
-        print("0 - Sair do sistema")
-        opcao = int(input("Escolha a opcao: "))
-        return opcao
+        # print("-------- oCurso ---------")
+        # print("Escolha sua opcao")
+        # print("1 - Usuario")
+        # print("2 - Cursos")
+        # print("3 - Progresso")
+        # print("9 - AUTOMATICO")
+        # print("0 - Sair do sistema")
+        # opcao = int(input("Escolha a opcao: "))
+        # return opcao
+        sg.ChangeLookAndFeel('DarkBlue')
+        botoes = [[sg.Button('Usuário', size=(20,2), key=1, button_color='#7B68EE')],
+                  [sg.Button('Cursos', size=(20,2), key=2)],
+                  [sg.Button('Progresso', size=(20,2), key=3)],
+                  [sg.Button('AUTOMATICO', size=(20,2), key=9)],
+                  [sg.Button('Sair do sistema', size=(20,2), key=0)]]
+
+        layout = [[sg.Text('oCurso', size=(15, 2), font=('Helvetica', 20), justification=('center'))],
+                  [sg.Column(botoes, vertical_alignment='center', justification='center', k='-C-')]]
+        
+        janela = sg.Window('oCurso').Layout(layout)
+        button, values = janela.read()
+        return button
 
     def mostra_mensagem(self, mensagem):
-        print(mensagem)
+        sg.Popup(mensagem)
