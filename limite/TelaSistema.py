931a699adb92dc3b48e30ae1c3cb8a30f4e4996f
jeffersonreis <33864468+jeffersonreis@users.noreply.github.com>
2021-09-17 00:05:56 -0300

    Ajuste tela Sistema
    Apenas mudei a descrição do botão para deslogar e acrescentei um titulo

--- limite/TelaSistema.py
+++ limite/TelaSistema.py
@@ -11,12 +11,12 @@ class TelaSistema():
                   [sg.Button('Cursos', size=(20,2), key=2, button_color='#7B68EE')],
                   [sg.Button('Progresso', size=(20,2), key=3, button_color='#7B68EE')],
                   [sg.Button('AUTOMATICO', size=(20,2), key=9, button_color='#7B68EE')],
-                  [sg.Button('Sair do sistema', size=(20,2), key=0)]]
+                  [sg.Button('Deslogar', size=(20,2), key=0)]]
 
         layout = [[sg.Text('oCurso', size=(15, 2), font=('Helvetica', 20), justification=('center'))],
                   [sg.Column(botoes, vertical_alignment='center', justification='center', k='-C-')]]
 
-        self.__window = sg.Window("Titulo", default_element_size=(40, 1)).Layout(layout)
+        self.__window = sg.Window("oCurso", default_element_size=(40, 1)).Layout(layout)
     
     def open(self):
         button, values = self.__window.Read()
